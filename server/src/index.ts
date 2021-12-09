9dae933f44aedc452f3fd219663317b90cc1b2a8
garricksu <garrickdsu@gmail.com>
2021-12-08 23:29:36 -0800

    switched to apollo-server-express for middleware use

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 import 'reflect-metadata'
+import express from 'express'
 import path from 'path'
 import { createConnection } from 'typeorm'
 import { User } from './entities/User'
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
 import { UserResolver } from './resolvers/user'
+import cors from 'cors'
 
 const main = async () => {
+  const app = express()
+
   const connection = await createConnection({
     type: 'postgres',
     database: 'twit',
@@ -18,14 +22,28 @@ const main = async () => {
     synchronize: true,
   })
 
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  )
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [UserResolver],
       validate: false,
     }),
+    context: ({ req }) => {
+      req
+    },
   })
 
-  apolloServer.listen(4000, () => {
+  await apolloServer.start()
+
+  apolloServer.applyMiddleware({ app })
+
+  app.listen(4000, () => {
     console.log('Apollo Server started on localhost:4000')
   })
 }
